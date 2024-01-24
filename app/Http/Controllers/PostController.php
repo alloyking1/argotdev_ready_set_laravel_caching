a9666b366c28be7b4782ba3bed290f0e6957efcd
alloyking1 <alloyking1@gmail.com>
2024-01-24 01:47:03 +0100

    refactor:post controller was refactored. It now has only two methods. One fetches data with caching the other feches data without caching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,63 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
-    public function index()
+    public function allCachedPost()
+    {
+
+        return Cache::remember('articles', 60, function () {
+            $post = Post::all();
+            return response()->json($post);
+        });
+    }
+
+    public function allPostWithoutCache()
     {
         $post = Post::all();
         return response()->json($post);
-    }
-
-    public function store(Request $request)
-    {
-        $validate = $request->validate(
-            [
-                'title' => 'string|require|max:100',
-                'body' => 'string|require',
-                'author' => 'string|require',
-            ]
-        );
-
-        Post::create([
-            'title' => $request->title,
-            'body' => $request->body,
-            'author' => $request->author,
-        ]);
-
-        return response()->json(['message' => 'Post created'], 201);
-    }
-
-    public function show($id)
-    {
-        $Post = Post::find($id);
-        if (!$post = null) {
-            return response()->json(['post' => $post], 200);
-        } else {
-            return response()->json([
-                'message' => "Post not found"
-            ], 404);
-        }
-    }
-
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'title' => 'required|max:255',
-            'body' => 'string|require',
-            'author' => 'string|require',
-        ]);
-        $post = Post::find($id);
-        $post->update($request->all());
-        return response()->json(['message' => 'Post updated successfully'],);
-    }
-
-    public function delete($id)
-    {
-        $post = Post::find($id);
-        $post->delete();
-
-        return response()->json(['message' => "post deleted"], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::prefix('/post')->group(function () {
-    Route::get('/', [PostController::class, 'index'])->name('post.all');
-    Route::post('/', [PostController::class, 'store'])->name('post.store');
-    Route::post('/show/{id}', [PostController::class, 'show'])->name('post.show');
-    Route::post('/update/{id}', [PostController::class, 'update'])->name('post.update');
-    Route::post('/delete/{id}', [PostController::class, 'delete'])->name('post.delete');
+    Route::get('/', [PostController::class, 'allCachedPost']);
+    Route::get('/', [PostController::class, 'allPostWithoutCache']);
 });
